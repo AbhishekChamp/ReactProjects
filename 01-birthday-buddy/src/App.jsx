9c98c45d0498feb1cef063876f4e8c9656831634
AbhishekChamp <abhishek96.r@gmail.com>
2023-03-29 17:09:20 +0530

    Project - 01 Complete

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./components/List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+    const [people, setPeople] = useState(data);
+
+    const togglePeople = (people) => {
+        console.log(people);
+        if (people.length === 0) {
+            return setPeople(data);
+        } else {
+            return setPeople([]);
+        }
+    };
+
+    return (
+        <main>
+            <section className='container'>
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button
+                    type='button'
+                    className='btn btn-block'
+                    onClick={() => togglePeople(people)}
+                >
+                    {people.length === 0 ? "Populate All" : "Clear All"}
+                </button>
+            </section>
+        </main>
+    );
 };
 export default App;
