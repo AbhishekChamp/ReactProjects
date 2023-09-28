9d900bba147ba2b32349f06a6714bf25eb099bfb
AbhishekChamp <abhishek96.r@gmail.com>
2023-09-28 19:56:26 +0530

    Project - 06 Part - 01

--- 06-menu/src/App.jsx
+++ 06-menu/src/App.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+import menu from "./data";
+import Title from "./Title";
+import Menu from "./Menu";
+import Categories from "../Categories";
+
+// const tempCategories = menu.map(item => item.category)
+// const tempSet = new Set(tempCategories)
+// const tempItems = ['all', ...tempSet]
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+    const [menuItem, setMenuItem] = useState(menu);
+    const [categories, setCategories] = useState(allCategories);
+    return (
+        <main className='menu'>
+            <Title text='our menu' />
+            <Categories categories={categories} />
+            <Menu items={menuItem} />
+        </main>
+    );
 };
 export default App;
