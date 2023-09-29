b544bbcc2712233d03ea98fe2fd7ce8d55249085
AbhishekChamp <abhishek96.r@gmail.com>
2023-09-30 00:06:46 +0530

    Project - 06 Functionality

--- 06-menu/src/App.jsx
+++ 06-menu/src/App.jsx
@@ -13,10 +13,21 @@ const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
     const [menuItem, setMenuItem] = useState(menu);
     const [categories, setCategories] = useState(allCategories);
+
+    const filterItems = (category) => {
+        if (category === "all") {
+            setMenuItem(menu);
+            return;
+        }
+
+        const newItems = menu.filter((item) => item.category === category);
+        setMenuItem(newItems);
+    };
+
     return (
         <main className='menu'>
             <Title text='our menu' />
-            <Categories categories={categories} />
+            <Categories categories={categories} filterItems={filterItems} />
             <Menu items={menuItem} />
         </main>
     );
