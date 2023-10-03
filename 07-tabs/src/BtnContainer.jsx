e42a526fc3821e480dc739d0b8c0c1f4f9b1ed17
AbhishekChamp <abhishek96.r@gmail.com>
2023-10-03 05:36:08 +0530

    Project - 07 Part - 1

---
+++ 07-tabs/src/BtnContainer.jsx
@@ -0,0 +1,13 @@
+function BtnContainer({ jobs }) {
+    return (
+        <div className='btn-container'>
+            {jobs.map((item) => (
+                <button key={item.id} className='job-btn'>
+                    {item.company}
+                </button>
+            ))}
+        </div>
+    );
+}
+
+export default BtnContainer;
