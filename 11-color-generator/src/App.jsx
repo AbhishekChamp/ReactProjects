bc4ada86878b76be33e754e27cd37d7ca6ed714c
AbhishekChamp <abhishek96.r@gmail.com>
2023-10-15 20:37:45 +0530

    Project - 11 Part - 2

--- 11-color-generator/src/App.jsx
+++ 11-color-generator/src/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import Values from "values.js";
+import Form from "./Form";
+import ColorList from "./ColorList";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+    const [colors, setColors] = useState(new Values("#f15025").all(10));
+    return (
+        <main>
+            <Form />
+            <ColorList />
+        </main>
+    );
 };
 export default App;
