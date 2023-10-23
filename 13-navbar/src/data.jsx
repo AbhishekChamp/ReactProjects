e1338b5a2c9c29675a7a13642e131683e2b67ea5
AbhishekChamp <abhishek96.r@gmail.com>
2023-10-23 12:49:39 +0530

    Project - 13 Starter

---
+++ 13-navbar/src/data.jsx
@@ -0,0 +1,51 @@
+import { FaGithub, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
+export const links = [
+    {
+        id: 1,
+        url: "/",
+        text: "home",
+    },
+    {
+        id: 2,
+        url: "/about",
+        text: "about",
+    },
+    {
+        id: 3,
+        url: "/projects",
+        text: "projects",
+    },
+    {
+        id: 4,
+        url: "/contact",
+        text: "contact",
+    },
+    {
+        id: 5,
+        url: "/profile",
+        text: "profile",
+    },
+];
+
+export const social = [
+    {
+        id: 1,
+        url: "https://www.instagram.com/coderdeck/",
+        icon: <FaInstagram />,
+    },
+    {
+        id: 2,
+        url: "https://www.youtube.com/@coderdeck",
+        icon: <FaYoutube />,
+    },
+    {
+        id: 3,
+        url: "https://www.linkedin.com/company/coderdeck-official/",
+        icon: <FaLinkedin />,
+    },
+    {
+        id: 4,
+        url: "https://github.com/AbhishekChamp",
+        icon: <FaGithub />,
+    },
+];
