18c94f7e11746445e9e976c5ace80c424859738c
AbhishekChamp <abhishek96.r@gmail.com>
2023-10-25 05:10:27 +0530

    Project - 13 Complete

--- 13-navbar/src/Navbar.jsx
+++ 13-navbar/src/Navbar.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 function Navbar() {
     const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
 
     const toggleLinks = () => {
         setShowLinks(!showLinks);
+    };
+
+    const linkStyles = {
+        height: showLinks
+            ? `${linksRef.current.getBoundingClientRect().height}px`
+            : `0px`,
     };
 
     return (
@@ -19,20 +27,32 @@ function Navbar() {
                         <FaBars />
                     </button>
                 </div>
-                {showLinks && (
-                    <div className='links-container'>
-                        <ul className='links'>
-                            {links.map((link) => {
-                                const { id, url, text } = link;
-                                return (
-                                    <li key={id}>
-                                        <a href={url}>{text}</a>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                )}
+                <div
+                    className='links-container'
+                    ref={linksContainerRef}
+                    style={linkStyles}
+                >
+                    <ul className='links' ref={linksRef}>
+                        {links.map((link) => {
+                            const { id, url, text } = link;
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{text}</a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className='social-icons'>
+                    {social.map((socialIcon) => {
+                        const { id, url, icon } = socialIcon;
+                        return (
+                            <li key={id}>
+                                <a href={url}>{icon}</a>
+                            </li>
+                        );
+                    })}
+                </div>
             </div>
         </nav>
     );
