72d32a8c44636d36f490fb5b424df8ce3b1cbc6b
AbhishekChamp <abhishek96.r@gmail.com>
2023-10-29 14:01:49 +0530

    Project - 14 Part - 02

--- 14-sidebar/src/Home.jsx
+++ 14-sidebar/src/Home.jsx
@@ -1,5 +1,18 @@
+import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
+
 function Home() {
-    return <div>Home</div>;
+    const { openSidebar, openModal } = useGlobalContext();
+    return (
+        <main>
+            <button onClick={openSidebar} className='sidebar-toggle'>
+                <FaBars />
+            </button>
+            <button onClick={openModal} className='btn'>
+                show modal
+            </button>
+        </main>
+    );
 }
 
 export default Home;
