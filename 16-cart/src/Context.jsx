ed075e6f45971471f35cbe371d4eaf591fa561a0
AbhishekChamp <abhishek96.r@gmail.com>
2023-11-16 05:29:43 +0530

    Project - 16 Part - 2

--- 16-cart/src/Context.jsx
+++ 16-cart/src/Context.jsx
@@ -1,6 +1,6 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 import reducer from "./reducer";
-
+import cartItems from "./data";
 import {
     CLEAR_CART,
     REMOVE,
@@ -14,13 +14,32 @@ const AppContext = createContext();
 
 const initialState = {
     loading: false,
-    cart: [],
+    cart: new Map(cartItems.map((item) => [item.id, item])),
 };
 
 export const AppProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    const clearCart = () => {
+        dispatch({ type: CLEAR_CART });
+    };
+
+    const remove = (id) => {
+        dispatch({ type: REMOVE, payload: { id } });
+    };
+
+    const increase = (id) => {
+        dispatch({ type: INCREASE, payload: { id } });
+    };
+
+    const decrease = (id) => {
+        dispatch({ type: DECREASE, payload: { id } });
+    };
+
     return (
-        <AppContext.Provider value={{ ...state }}>
+        <AppContext.Provider
+            value={{ ...state, clearCart, remove, increase, decrease }}
+        >
             {children}
         </AppContext.Provider>
     );
