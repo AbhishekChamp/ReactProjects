a3abf9c85625f2b694607f793aa4cef10369422e
AbhishekChamp <abhishek96.r@gmail.com>
2023-11-26 12:10:00 +0530

    Project - 17 Part-5

--- 17-tasks-app/frontend/src/Items.jsx
+++ 17-tasks-app/frontend/src/Items.jsx
@@ -1,25 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
 import SingleItem from "./SingleItem";
-import customFetch from "./utils";
+import { useFetchTasks } from "./reactQueryCustomHooks";
 
 const Items = () => {
-    const { isLoading, data, isError, error } = useQuery({
-        queryKey: ["tasks"],
-        queryFn: async () => {
-            const { data } = await customFetch.get("/");
-            return data;
-        },
-    });
+    const { isLoading, data, isError } = useFetchTasks();
 
     if (isLoading) {
         return <p style={{ marginTop: "1rem" }}>Loading...</p>;
     }
 
-    // if (isError) {
-    //     return <p style={{marginTop: '1rem'}}>There was an error...</p>
-    // }
-
-    if (error) {
+    if (isError) {
         return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>;
     }
 
